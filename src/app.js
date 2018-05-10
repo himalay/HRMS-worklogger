d8111fb7462a72657578391ef6526813e498b999
Himalay <contact@himalay.com.np>
2018-05-10 22:08:48 +0545

    Add puppeteer

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('Yay!')
+import puppeteer from 'puppeteer'
+
+const { NODE_ENV, HRMS_URL, HRMS_USERNAME, HRMS_PASSWORD } = process.env
+
+;(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: NODE_ENV === 'production' })
+    const page = await browser.newPage()
+    await page.goto(HRMS_URL)
+    await page.waitForSelector('#form1')
+    await page.type('#txtUserName', HRMS_USERNAME)
+    await page.type('#txtPassword', HRMS_PASSWORD)
+    await page.click('#btnLogin')
+    await page.waitForSelector('#ctl00_HeaderCtl1_rptMainMenu_ctl08_rpt1_ctl00_HyperLink1')
+    await page.click('#ctl00_HeaderCtl1_rptMainMenu_ctl08_rpt1_ctl00_HyperLink1')
+
+    // await browser.close()
+  } catch (error) {
+    console.error(error)
+  }
+})()
